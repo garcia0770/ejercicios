76aa96af2ee4f57dce610a687ff7d1a1e2cb9e13
garcia0770 <ocamporestrepo23@gmail.com>
2024-07-09 19:45:23 -0500

    Update ejercicios_POO.py
    Agregamos las opciones de prestramos, tomar prestado, devolver publicaciones

--- ejercicios_POO.py
+++ ejercicios_POO.py
@@ -26,4 +26,21 @@ class Revista(Publicacion):
 
     def mostrar_informacion(self):
         print(f"Revista: {self._titulo}, Autor: {self._autor}, Año: {self._año}, número de Edicion: {self._numero_edicion} ")
-        
+
+class Usuario: 
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self._prestamos = []
+
+    def tomar_prestamo(self, publicacion):
+        self._prestamos.append(publicacion)
+        print(f"self.nombre ha tomado prestado'{publicacion._titulo}'")
+
+    def devolver_publicacion(self, publicacion):
+        if publicacion in self._prestamos:
+            self._prestamos.remove(publicacion)
+            print(f"{self.nombre} ha devuelto 'publicacion_titulo'")
+        else:
+                print (f"{self.nombre} no ha tomado prestado los siguientes titulos:")
+                for publicacion in self._prestamos:
+                    publicacion.mostrar_informacion()
